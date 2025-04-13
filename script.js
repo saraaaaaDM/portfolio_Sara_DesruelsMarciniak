34ba8bfe073fb588744fdd06cd30f29d724defa9
saraaaaaDM <desruelsarapro@gmail.com>
2025-04-13 15:44:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.getElementById("contact-form").addEventListener("submit", function(e) {
   e.preventDefault();
   document.querySelector(".form-message").textContent = "Message envoyé avec succès !";
