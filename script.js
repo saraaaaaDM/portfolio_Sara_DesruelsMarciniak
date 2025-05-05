d84f8a93a8c0f4cc4d1ea61c3e7992355611b5f2
saraaaaaDM <desruelsarapro@gmail.com>
2025-05-05 10:22:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
 document.getElementById("contact-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.querySelector(".form-message").textContent = "Message envoyé avec succès !";
-  this.reset();
-});
-
-
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  const hero = document.querySelector('.hero');
+    e.preventDefault();
+    document.querySelector(".form-message").textContent = "Message envoyé avec succès !";
+    this.reset();
+  });
   
-  if (window.scrollY > hero.offsetHeight) {
-    navbar.classList.add('scrolled'); 
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
+  window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    const hero = document.querySelector('.hero');
+    const logoImg = document.querySelector('.logo-img');
+  
+    if (window.scrollY > hero.offsetHeight) {
+      navbar.classList.add('scrolled');
+      logoImg.src = "logo-blanc.png"; // logo après scroll
+    } else {
+      navbar.classList.remove('scrolled');
+      logoImg.src = "logo-rouge.png"; // logo dans le Hero
+    }
+  });
