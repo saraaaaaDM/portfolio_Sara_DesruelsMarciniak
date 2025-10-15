9eccfa443b5c1ef23e9a3b3a5727f7efa78b3161
saraaaaaDM <desruelsarapro@gmail.com>
2025-10-15 15:01:30 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const track = document.querySelector('.carousel-track');
+  const slides = Array.from(track.children);
+  const nextButton = document.querySelector('.carousel-btn.next');
+  const prevButton = document.querySelector('.carousel-btn.prev');
+
+  let currentIndex = 0;
+
+  function updateCarousel() {
+    track.style.transform = 'translateX(-' + currentIndex * 100 + '%)';
+  }
+
+  nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+  });
+
+  prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
+  });
+
+  
+});
+
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     const hero = document.querySelector('.hero1');
